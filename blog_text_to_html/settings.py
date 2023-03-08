844feab29770655c4f63a18fb49e990bba5be8ec
PeterOlayemi <olayemipeter2005@gmail.com>
2023-03-08 18:54:40 +0100

    new commit

--- blog_text_to_html/settings.py
+++ blog_text_to_html/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rpx@l9r)ubk=g4uizb8c-fcc_kuepu1-*ff$6n2jsfsie^ksn-'
+
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
